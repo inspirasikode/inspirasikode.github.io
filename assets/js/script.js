3ce0607cc5ab595ae120101d5e615805ef98307a
inspirasikode <inspirasikode@gmail.com>
2021-04-10 16:38:12 +0700

    create ui

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// tab
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("mainin-tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+  
+document.getElementById("defaultOpen").click();
